99ba7713c6b8e6b49811f9a399b8f299e5d100c7
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-05-16 12:29:45 +0000

    Modify src folder files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './style.css';
+import scores from './module/database';
+
+const container = document.querySelector('.container');
+
+let displayScore = () => {
+  container.innerHTML = '';
+  for (let i = 0; i < scores.length; i += 1) {
+    container.innerHTML += `
+    <li class="${scores[i].id % 2 ? "odd" : "even"}">${scores[i].name} : ${scores[i].score}</li>
+    </ul>
+    `;
+  }
+};
+
+displayScore();
