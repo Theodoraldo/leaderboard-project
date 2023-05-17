519b0a169aea50136b3a8f6af48da196db7832e0
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-05-17 20:04:09 +0000

    Modify data fetch from API

--- src/module/saveToDb.js
+++ src/module/saveToDb.js
@@ -4,23 +4,24 @@ let sliceId = '';
 const userName = document.getElementById('user_name');
 const userScore = document.getElementById('user_score');
 
-if (sliceId !== []) {
-  sliceId = localStorage.getItem('generatedId').slice(14, 34);
-}
+
 
 const saveScore = async () => {
-  await fetch(`${leaderBoardUrl}games/${sliceId}/scores/`, {
-    method: 'POST',
-    body: JSON.stringify({
-      user: userName.value,
-      score: userScore.value,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  userName.value = '';
-  userScore.value = '';
+  if (sliceId !== []) {
+    sliceId = localStorage.getItem('generatedId').slice(14, 34);
+    await fetch(`${leaderBoardUrl}games/${sliceId}/scores/`, {
+      method: 'POST',
+      body: JSON.stringify({
+        user: userName.value,
+        score: userScore.value,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    userName.value = '';
+    userScore.value = '';
+  }
 };
 
 export default saveScore;
