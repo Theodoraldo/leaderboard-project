7f6c917621b774e7af768683b01cd61727d9a92f
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-05-17 19:41:31 +0000

    Add data to API JS file

---
+++ src/module/saveToDb.js
@@ -0,0 +1,26 @@
+const leaderBoardUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+
+let sliceId = '';
+const userName = document.getElementById('user_name');
+const userScore = document.getElementById('user_score');
+
+if (sliceId !== []) {
+  sliceId = localStorage.getItem('generatedId').slice(14, 34);
+}
+
+const saveScore = async () => {
+  await fetch(`${leaderBoardUrl}games/${sliceId}/scores/`, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: userName.value,
+      score: parseInt(userScore.value),
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  userName.value = '';
+  userScore.value = '';
+};
+
+export default saveScore;
